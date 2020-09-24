f73dabac73c3b98552aa0637cb0e9b79c7a04f1d
Princend <waxdji3g4y3c04@gmail.com>
2020-09-24 22:06:45 +0800

    first

---
+++ todo.js
@@ -0,0 +1,39 @@
+var data = {
+    a: 1,
+    rawHtml: '<span style="color:red">this is shuld be</span>',
+    color: 'blue',
+    isActive: true
+};
+
+
+var beforeCreate = function () { console.log('beforeCreate'); }
+var created = function () { console.log('created'); }
+var beforeMount = function () { console.log('beforeMount'); }
+var mounted = function () { console.log('mounted'); }
+var beforeUpdate = function () { console.log('beforeUpdate'); }
+var updated = function () { console.log('updated'); }
+var click1 = function () { console.log('click1'); }
+var click2 = function () { console.log('click2'); }
+var vm = new Vue({
+    el: '#app',
+    data: data,
+    methods: { click1: click1, click2: click2 },
+    beforeCreate: beforeCreate,
+    created: created,
+    beforeMount: beforeMount,
+    mounted: mounted,
+    beforeUpdate: beforeUpdate,
+    updated: updated,
+},
+
+)
+
+vm.$watch('a', (newval, oldval) => {
+    console.log(newval, oldval);
+})
+vm.$data.a = 'test';
+setTimeout(() => {
+    vm.a = 'change...'
+}, 2000)
+
+
